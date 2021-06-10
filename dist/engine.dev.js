b5f9a27a07a0045670dc544467af158a51870f64
Rahul Chandankeri <77581043+Rahulchandankeri@users.noreply.github.com>
2021-06-10 17:10:06 +0530

    FINAL

---
+++ dist/engine.dev.js
@@ -0,0 +1,26 @@
+"use strict";
+
+function dec() {
+  document.querySelector(".getpeople").stepDown(1);
+}
+
+function inc() {
+  document.querySelector(".getpeople").stepUp(1);
+}
+
+function get() {
+  var total = document.getElementById("billamount").value; // Get input bill amount
+
+  var inp = document.getElementById("servicequality").value; // Get tip percantage input
+
+  var inpeople = document.getElementById("cvalue").value; // Get input of number of people spliting bill
+
+  if (total == "" | total <= 0) {
+    alert("It seems your entered invalid amount");
+    location.reload();
+  } else {
+    var split = total * inp / inpeople; // Maths op
+
+    document.getElementById("result").innerHTML = "Tip amount to be paid by each person " + " " + split; // Display results
+  }
+}
--- engine.js
+++ engine.js
@@ -12,9 +12,15 @@ function get() {
     var inp = document.getElementById("servicequality").value; // Get tip percantage input
     var inpeople = document.getElementById("cvalue").value;  // Get input of number of people spliting bill
 
+    if(total == "" | total <= 0 ){
+        alert("It seems your entered invalid amount");
+        location.reload();
+    }
+    else {
     var split = (total * inp / inpeople);  // Maths op
-
     document.getElementById("result").innerHTML = "Tip amount to be paid by each person " + " " + split;  // Display results
+
+    }
 
 
 
